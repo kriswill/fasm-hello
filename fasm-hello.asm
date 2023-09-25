b7028b4db305c6e76a1764b5a7d9e46641181801
Kris Williams <115474+kriswill@users.noreply.github.com>
2023-09-24 18:28:08 -0700

    improve hello world program

--- fasm-hello.asm
+++ fasm-hello.asm
@@ -1,11 +1,13 @@
 format ELF64 executable 3
 
+; == constants ==
+; Linux Syscalls
+; see: https://chromium.googlesource.com/chromiumos/docs/+/master/constants/syscalls.md#x86_64-64_bit
 SYS_WRITE equ 1
 SYS_EXIT  equ 60
-ENDL      equ 10
+ENDL      equ 10 ; '\n'
 
-macro syscall number, arg1, arg2, arg3
-{
+macro syscall number, arg1, arg2, arg3 {
   mov rax, number
   mov rdi, arg1
   mov rsi, arg2
@@ -13,13 +15,21 @@ macro syscall number, arg1, arg2, arg3
   syscall
 }
 
+macro write message, length {
+  syscall SYS_WRITE, 1, message, length
+}
+
+macro exit {
+  syscall SYS_EXIT, 0, 0, 0
+}
+
 ; == start of program ==
 segment readable executable
 entry main
 
 main:
-  syscall SYS_WRITE, 1, message, message_length
-  syscall SYS_EXIT, 0, 0, 0
+  write message, message_length
+  exit
 
 ; == data segment ==
 segment readable writeable
